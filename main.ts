99d029ec91e829fa73b37a273138772e4f4af009
Yu Wang <yuwan@microsoft.com>
2022-03-10 11:06:49 -0800

    refactor: async dict loading & simplify CodeMirror object retrieval

--- main.ts
+++ main.ts
@@ -1,42 +1,46 @@
-import { Editor, Plugin, TFile } from 'obsidian';
+import { Editor, Notice, Plugin } from 'obsidian';
 
 declare const CodeMirror: any;
+declare module "obsidian" {
+	interface Vault{
+		getConfig(config_name: string): any;
+	}
+}
 
 export default class VimMultibyteCharSearchPlugin extends Plugin {
 	private code_mirror_vim_object: any = null;
-	private initialized = false;
 	private complex_char_2_simple_char_mapping: ComplexChar2SimpleCharMapping = null;
 
-	async initialize() {
-		if (this.initialized)
-			return;
-
-		// Determine if we have the legacy Obsidian editor (CM5) or the new one (CM6).
-		// This is only available after Obsidian is fully loaded, so we do it as part of the `file-open` event.
-		if ('editor:toggle-source' in (this.app as any).commands.editorCommands) {
-			this.code_mirror_vim_object = (window as any).CodeMirrorAdapter?.Vim;
-			console.log('Vimrc plugin: using CodeMirror 6 mode');
-		} else {
+	initialize() {
+		const is_use_legacy_editor = this.app.vault.getConfig('legacyEditor') as boolean
+		if(is_use_legacy_editor) {
 			this.code_mirror_vim_object = CodeMirror.Vim;
 			console.log('Vimrc plugin: using CodeMirror 5 mode');
-		}
+		} else {
+			this.code_mirror_vim_object = (window as any).CodeMirrorAdapter?.Vim;
+			console.log('Vimrc plugin: using CodeMirror 6 mode');
+		} 
+	}
 
-		this.initialized = true;
+	async load_dict() {
+		const dict_content = await this.app.vault.adapter.read(".obsidian/plugins/obsidian-vim-multibyte-char-search/pinyin_search.dict.txt");
+		this.complex_char_2_simple_char_mapping = new ComplexChar2SimpleCharMapping(dict_content);
 	}
 
 	async onload() {
-		this.app.workspace.on('file-open', async (file: TFile) => {
-			if (!this.initialized)
-				await this.initialize();
-		});
+		this.app.workspace.onLayoutReady(this.initialize.bind(this));
 
-		const dict_content = await this.app.vault.adapter.read(".obsidian/plugins/obsidian-vim-multibyte-char-search/pinyin_search.dict.txt")
-		this.complex_char_2_simple_char_mapping = new ComplexChar2SimpleCharMapping(dict_content);
+		this.load_dict();
 
 		this.addCommand({
 			id: 'enrich-current-vim-search-pattern',
 			name: 'Enrich Current VIM Search Pattern',
 			editorCallback: (editor: Editor) => {
+				if(this.complex_char_2_simple_char_mapping == null)
+				{
+					new Notice("Complex char to simple char dict is not ready yet.")
+					return;
+				}
 				const vim_global_state = this.code_mirror_vim_object.getVimGlobalState_();
 				const vim_search_pattern = vim_global_state.query;
 				if(vim_search_pattern == null)
